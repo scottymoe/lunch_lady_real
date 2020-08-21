1e65fc86a8dc16676b4ed347d795c0b423f40825
Scott Mohorn <scottmohorn@gmail.com>
2020-08-21 11:19:11 -0600

    Added basic functionality

---
+++ app.rb
@@ -0,0 +1,6 @@
+require_relative "customer"
+
+test_customer = Customer.new(100, "Bob")
+
+puts test_customer.name
+puts test_customer.wallet_balance
---
+++ customer.rb
@@ -0,0 +1,7 @@
+class Customer
+  attr_accessor :wallet_balance, :name
+  def initialize(wallet_balance, name)
+    @wallet_balance = wallet_balance
+    @name = name
+
+end
